88a556985b6b6079af7ce51897048451af7a5cca
Pradeep Kushwaha <prk.shine@gmail.com>
2020-03-13 21:08:07 +0530

    Updates

---
+++ precache-manifest.f3984fbe71ca73ac5e7ee3be53943b19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975743fff769795c70c72daa90c53d08",
+    "url": "/robotboy/index.html"
+  },
+  {
+    "revision": "3e57aed984826c29aad7",
+    "url": "/robotboy/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8316ed6a7ef9d3f7fe00",
+    "url": "/robotboy/static/css/main.2c69a40b.chunk.css"
+  },
+  {
+    "revision": "3e57aed984826c29aad7",
+    "url": "/robotboy/static/js/2.5b07d1dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotboy/static/js/2.5b07d1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8316ed6a7ef9d3f7fe00",
+    "url": "/robotboy/static/js/main.c6cff87b.chunk.js"
+  },
+  {
+    "revision": "d5ee54d5ef7435e5e217",
+    "url": "/robotboy/static/js/runtime-main.54fc5cb7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotboy/static/media/SEGA.12a3db8b.woff"
+  }
+]);
